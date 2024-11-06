6e0ddd9a7afd234ec3d9cf5e2666d036536d6106
alsaegh98 <abdullajaffer8@gmail.com>
2024-11-06 13:03:54 +0300

    done this lab

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,46 @@
-import { useState } from 'react'
+import { useState } from 'react';
+import IngredientList from './components/IngredientList';
+import BurgerStack from './components/BurgerStack';
 import './App.css';
 
-export const availableIngredients = [
-  { name: 'Kaiser Bun', color: 'saddlebrown' },
-  { name: 'Sesame Bun', color: 'sandybrown' },
-  { name: 'Gluten Free Bun', color: 'peru' },
-  { name: 'Lettuce Wrap', color: 'olivedrab' },
-  { name: 'Beef Patty', color: '#3F250B' },
-  { name: 'Soy Patty', color: '#3F250B' },
-  { name: 'Black Bean Patty', color: '#3F250B' },
-  { name: 'Chicken Patty', color: 'burlywood' },
-  { name: 'Lettuce', color: 'lawngreen' },
-  { name: 'Tomato', color: 'tomato' },
-  { name: 'Bacon', color: 'maroon' },
-  { name: 'Onion', color: 'lightyellow' },
-  { name: 'Cheddar Cheese', color: '#FDE18B' },
-  { name: 'Swiss Cheese', color: '#F1E1A8' },
-];
-
 const App = () => {
+  const [stack, setStack] = useState([]);
+
+  const availableIngredients = [
+    { name: 'Kaiser Bun', color: 'saddlebrown' },
+    { name: 'Sesame Bun', color: 'sandybrown' },
+    { name: 'Gluten Free Bun', color: 'peru' },
+    { name: 'Lettuce Wrap', color: 'olivedrab' },
+    { name: 'Beef Patty', color: '#3F250B' },
+    { name: 'Soy Patty', color: '#3F250B' },
+    { name: 'Black Bean Patty', color: '#3F250B' },
+    { name: 'Chicken Patty', color: 'burlywood' },
+    { name: 'Lettuce', color: 'lawngreen' },
+    { name: 'Tomato', color: 'tomato' },
+    { name: 'Bacon', color: 'maroon' },
+    { name: 'Onion', color: 'lightyellow' },
+    { name: 'Cheddar Cheese', color: '#FDE18B' },
+    { name: 'Swiss Cheese', color: '#F1E1A8' },
+
+  ];
+
+  const addToBurger = (ingredient) => {
+    setStack([...stack, ingredient]);
+  };
+
+  const removeFromBurger = (index) => {
+    const newStack = stack.filter((_, i) => i !== index);
+    setStack(newStack);
+  };
+
   return (
     <main>
       <h1>Burger Stacker</h1>
       <section>
-      {/* List & Stack components */}
+        <dev>
+        <IngredientList ingredients={availableIngredients} addToBurger={addToBurger} />
+        </dev>
+        <BurgerStack stack={stack} removeFromBurger={removeFromBurger} />
       </section>
     </main>
   );
